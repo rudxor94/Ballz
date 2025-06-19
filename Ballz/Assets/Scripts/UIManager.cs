882cef4334fc531b00f68e960549e59d50b51749
rudxor94 <rudxor94@naver.com>
2025-06-19 23:49:33 +0900

    UI 껐다키는 것과 스코어 업데이트 부분 분리
    UI 껐다키는 것과 스코어 업데이트 부분 분리

--- Ballz/Assets/Scripts/UIManager.cs
+++ Ballz/Assets/Scripts/UIManager.cs
@@ -14,6 +14,7 @@ public class UIManager : MonoBehaviour
 
     private int score;
     private int highScore;
+    private GameManager.State state;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -24,67 +25,78 @@ public class UIManager : MonoBehaviour
 
         highScore = PlayerPrefs.GetInt("HighScore", 0);
         highScoreText.text = highScore.ToString();
+        state = GameManager.State.Play;
     }
 
     private void Update()
     {
-        switch (GameManager.Instance.state)
+        if (state != GameManager.Instance.state)
         {
-            case GameManager.State.Title:
-                {
-                    titleUI.SetActive(true);
-                    menuUI.SetActive(false);
-                    playUI.SetActive(false);
-                    endUI.SetActive(false);
-                }
-                break;
-
-            case GameManager.State.Play:
-            case GameManager.State.BallMove:
-            case GameManager.State.LineMove:
-                {
-                    titleUI.SetActive(false);
-                    menuUI.SetActive(false);
-                    playUI.SetActive(true);
-                    endUI.SetActive(false);
-                    var score = GameManager.Instance.GetScore();
-                    if (this.score != score)
+            switch (GameManager.Instance.state)
+            {
+                case GameManager.State.Title:
                     {
-                        scoreText.text = "";
-                        scoreText.text = score.ToString();
-                        scoreText.ForceMeshUpdate(true);
-                        if (score > highScore)
-                        {
-                            highScoreText.text = "";
-                            highScoreText.text = score.ToString();
-                            highScore = score;
-                            highScoreText.ForceMeshUpdate(true);
-                        }
+                        titleUI.SetActive(true);
+                        menuUI.SetActive(false);
+                        playUI.SetActive(false);
+                        endUI.SetActive(false);
                     }
-                }
-                break;
+                    break;
 
-            case GameManager.State.Pause:
+                case GameManager.State.Play:
+                case GameManager.State.BallMove:
+                case GameManager.State.LineMove:
+                    {
+                        titleUI.SetActive(false);
+                        menuUI.SetActive(false);
+                        playUI.SetActive(true);
+                        endUI.SetActive(false);
+                    }
+                    break;
+
+                case GameManager.State.Pause:
+                    {
+                        titleUI.SetActive(false);
+                        menuUI.SetActive(true);
+                        playUI.SetActive(false);
+                        endUI.SetActive(false);
+                    }
+                    break;
+
+                case GameManager.State.End:
+                    {
+                        titleUI.SetActive(false);
+                        menuUI.SetActive(false);
+                        playUI.SetActive(false);
+                        endUI.SetActive(true);
+
+                        var score = GameManager.Instance.GetScore();
+                        endScoreText.text = score.ToString();
+                        endHighScoreText.text = $"BEST {highScore}";
+                    }
+                    break;
+            }
+
+            state = GameManager.Instance.state;
+        }
+
+        if (state == GameManager.State.Play || state == GameManager.State.BallMove || state == GameManager.State.LineMove)
+        {
+            var score = GameManager.Instance.GetScore();
+            if (this.score != score)
+            {
+                Canvas.ForceUpdateCanvases();
+                scoreText.text = "";
+                scoreText.text = score.ToString();
+                scoreText.ForceMeshUpdate(true);
+                if (score > highScore)
                 {
-                    titleUI.SetActive(false);
-                    menuUI.SetActive(true);
-                    playUI.SetActive(false);
-                    endUI.SetActive(false);
+                    highScoreText.text = "";
+                    highScoreText.text = score.ToString();
+                    highScore = score;
+                    highScoreText.ForceMeshUpdate(true);
                 }
-                break;
-
-            case GameManager.State.End:
-                {
-                    titleUI.SetActive(false);
-                    menuUI.SetActive(false);
-                    playUI.SetActive(false);
-                    endUI.SetActive(true);
-
-                    var score = GameManager.Instance.GetScore();
-                    endScoreText.text = score.ToString();
-                    endHighScoreText.text = $"BEST {highScore}";
-                }
-                break;
+            }
         }
     }
 }
